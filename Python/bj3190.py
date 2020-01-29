2719930b8e99ec078c6a59f2ec637d8daacd0d05
Dohee Yun <dreamingcats2@gmail.com>
2020-01-29 17:57:56 +0900

    200129 | try to solve bj3190

---
+++ Python/bj3190.py
@@ -0,0 +1,43 @@
+# 백준알고리즘 뱀
+
+import sys
+sys.stdin = open('bj3190.txt', 'r')
+
+
+for case in range(3):
+    N, K = int(input()), int(input())
+    for i in range(K):
+
+
+# 6 // N
+# 3
+# 3 4 // 사과의 위치
+# 2 5
+# 5 3
+# 3
+# 3 D
+# 15 L
+# 17 D
+# 10
+# 4
+# 1 2
+# 1 3
+# 1 4
+# 1 5
+# 4
+# 8 D
+# 10 D
+# 11 D
+# 13 L
+# 10
+# 5
+# 1 5
+# 1 3
+# 1 2
+# 1 6
+# 1 7
+# 4
+# 8 D
+# 10 D
+# 11 D
+# 13 L
